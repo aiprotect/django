75d3bf60f9ed315945790fddaf4a72134f86522c
aiprotect <qrbashayan@gmail.com>
2025-04-25 21:13:22 +0330

    Update urls.py

--- github/urls.py
+++ github/urls.py
@@ -5,5 +5,5 @@ from .views import GitHubView, SisterView
 
 urlpatterns = [
     path('', GitHubView.as_view(), name='github-home'),
-    path('sister/, SisterView.as_view(), name='sister-page '
+    path('sister/, SisterView.as_view(), name='sister-page')
 ]
