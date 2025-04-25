8150849b0e8872be155e5008a1165648e3cb0c89
aiprotect <qrbashayan@gmail.com>
2025-04-25 21:09:12 +0330

    Update urls.py

--- github/urls.py
+++ github/urls.py
@@ -1,8 +1,9 @@
 
 
 from django.urls import path
-from .views import GitHubView
+from .views import GitHubView, SisterView
 
 urlpatterns = [
-    path('', GitHubView.as_view(), name='github-home')
+    path('', GitHubView.as_view(), name='github-home'),
+    path('sister/, SisterView.as_view(), name='sister-page '
 ]
